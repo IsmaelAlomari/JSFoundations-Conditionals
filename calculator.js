a772f578cd74b531b3b8f958b2cf2a2969538b8d
IsmaelAlomari <ismaelalomari994@gmail.com>
2021-05-18 10:26:37 +0300

    YAAAAS

--- calculator.js
+++ calculator.js
@@ -2,5 +2,26 @@ const prompt = require("prompt-sync")(); // Please don't touch me :)
 
 // The prompt method allows us to take an input from the user.
 // Run the code to see how to use prompt.
-const name = prompt("What's your name? ");
-console.log(`Hi, I'm ${name}.`);
+//const name = prompt("What's your name? ");
+//console.log(`Hi, I'm ${name}.`);
+
+let = prompt("enter first num1");
+let  = prompt ("enter second num2");
+ num1 =  (5 )
+ num2 =  (3)
+
+ let opreater  = prompt ("Please choose the operation (+, -, /, *)")
+
+if (opreater === "+") {
+    console.log (num1 + num2)
+}
+ else if (opreater === "-") {
+     console.log (num1 - num2)
+ }
+ else if ( opreater === "*") {
+     console.log ( num1 * num2 ) 
+ }
+
+ else if(opreater === "/") {
+     console.log ( num1 /num2)
+ }
